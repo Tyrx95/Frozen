68b2ea4612de6ec75b5bfa13b8afed779b273572
ekalac1 <ekalac1@etf.unsa.ba>
2017-07-17 12:44:13 +0200

    Inital commit

---
+++ demo/src/main/java/com/example/demo/FrozenSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrozenSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrozenSpringBootApplication.class, args);
+	}
+}
