3a305dccfdc7de161f9315a9f730a24a88b6ef65
Tyrx95 <amartiric@gmail.com>
2017-07-26 12:42:04 +0200

    Date insertion implemented

--- demo/src/main/java/com/example/demo/service/ViberBotServiceImpl.java
+++ demo/src/main/java/com/example/demo/service/ViberBotServiceImpl.java
@@ -14,12 +14,15 @@ import com.viber.bot.event.incoming.IncomingUnsubscribeEvent;
 import java.util.Optional;
 import java.util.concurrent.Future;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.database.Room;
@@ -40,14 +43,18 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 
 @Service
 public class ViberBotServiceImpl implements ViberBotService {
-	
+
 	@Autowired
 	private UserService userService;
 
 	@Autowired
 	private RoomService roomService;
-
-
+	
+	String roomNumber;
+	
+	
+	LocalDate date;
+	
 
 	@Override
 	public void onMessageReceived(ViberBot bot) {
@@ -55,42 +62,72 @@ public class ViberBotServiceImpl implements ViberBotService {
 		bot.onMessageReceived((new OnMessageReceived() {
 			@Override
 			public void messageReceived(IncomingMessageEvent event, Message message, Response response) {
-				
-				
-				if(message.getTrackingData().get("message").equals("welcome_message")) {
-					
-					if(message.getMapRepresentation().get("text").equals("Reserve room")){
-							MessageKeyboard roomKeyboard = createRoomKeyboard();
-							Map<String, Object> trDataHMap = new HashMap<>();
-							trDataHMap.put("message", "get_rooms");
-							TrackingData trData=new TrackingData(trDataHMap);
 
-							response.send(new TextMessage("Please select one of the available rooms."
-									,roomKeyboard, trData, new Integer(1))
-									);
+				String msg=message.getTrackingData().get("message").toString();
+				String messageText=message.getMapRepresentation().get("text").toString();
+				
+				if (messageText.equals("Cancel")) {
+					response.send(welcomeMessage(event.getSender().getName()));
+				}
+
+				else if (msg.equals("welcome_message")) {
+
+					if (messageText.equals("Reserve room")) {
+						MessageKeyboard roomKeyboard = createRoomKeyboard();
+						Map<String, Object> trDataHMap = new HashMap<>();
+						trDataHMap.put("message", "get_rooms");
+						TrackingData trData = new TrackingData(trDataHMap);
+
+						response.send(new TextMessage("Please select one of the available rooms.", roomKeyboard, trData,
+								new Integer(1)));
+					} else if (messageText.equals("See previous reservations")) {
+
 					}
-					else if(message.getMapRepresentation().get("text").equals("See previous reservations")){
-							
-							}
+
+				}
+
+				else if (msg.equals("get_rooms")) {
+
+					Map<String, Object> trDataHMap = new HashMap<>();
+					trDataHMap.put("message", "enter_date");
+					roomNumber = messageText.split("\\s+")[1];
+					System.out.println(roomNumber);
+					TrackingData trData = new TrackingData(trDataHMap);
+
+					response.send(
+							new TextMessage("Enter the desired date in format : dd.MM.yyyy", createCancelKeyboard(), trData, new Integer(1)));
+
+				}
+				
+				else if (msg.equals("enter_date")) {
+
+					try{
+						
+						DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+						LocalDate dateX = LocalDate.parse(messageText, formatter);
+						date=dateX;
+						System.out.println(dateX);
+						System.out.println(date);
+						System.out.println("FOrmat is good ");
+						System.out.println(date);
+						
+					}
+					catch(Exception e)
+					{	
+						System.out.println(date);
+						System.out.println(messageText);
+
+						response.send(new TextMessage("Invalid format. Please enter the date again in format : dd.MM.yyyy",
+								createCancelKeyboard(),
+								message.getTrackingData(),
+								new Integer(1)));
+					}
 					
+
 				}
 				
 				
-				else if(message.getTrackingData().get("message").equals("get_rooms")) {
-					
-					
-					System.out.println("Listing rooms.. ");
-					
-					
-					
-					
-				}				
 				
-				
-				
-				
-				
-
 			}
 
 		}));
@@ -107,7 +144,7 @@ public class ViberBotServiceImpl implements ViberBotService {
 
 				if (userService.getByViberId(userPr.getId()) == null) {
 					userService.add(new User(userPr.getId(), userPr.getName(), true));
-					
+
 				}
 
 				else {
@@ -147,38 +184,7 @@ public class ViberBotServiceImpl implements ViberBotService {
 			@Override
 			public Future<Optional<Message>> conversationStarted(IncomingConversationStartedEvent event) {
 
-				String JSONString = "{\"Type\": \"keyboard\",\"Buttons\": [{\"Columns\": 3,\"Rows\": 2,\"Text\": \"<font color=\\\"#494E67\\\">Reserve room </font><br><br>\",\"TextSize\": \"medium\",\"TextHAlign\": \"center\",\"TextVAlign\": \"bottom\",\"ActionType\": \"reply\",\"ActionBody\": \"Reserve room\",\"BgColor\": \"#f7bb3f\"}, {\"Columns\": 3,\"Rows\": 2,\"Text\": \"<font color=\\\"#494E67\\\">See previous reservations</font><br><br>\",\"TextSize\": \"medium\",\"TextHAlign\": \"center\",\"TextVAlign\": \"bottom\",\"ActionType\": \"reply\",\"ActionBody\": \"See previous reservations\",\"BgColor\": \"#f6f7f9\"}]}";
-				String keyboardMessageStr = JSONString;
-				
-
-				try {
-					MessageKeyboard messageKeyboard = new MessageKeyboard(
-							new ObjectMapper().readValue(keyboardMessageStr, new TypeReference<Map<String, Object>>() {
-							}));
-					
-					Map<String, Object> trDataHMap = new HashMap<>();
-					trDataHMap.put("message", "welcome_message");
-					
-					TrackingData trData=new TrackingData(trDataHMap);
-					String text="Hello  , " + event.getUser().getName()+
-							". Welcome to ViberBot room reservation service .Please select one of the following options.";
-					Integer minApiVersion=new Integer(1);
-					TextMessage txtMessage = new TextMessage(text,
-							messageKeyboard, trData,minApiVersion);
-					return Futures.immediateFuture(Optional.of(txtMessage));
-
-				} catch (JsonParseException e) {
-					System.out.println("Error in parsing");
-					e.printStackTrace();
-				} catch (JsonMappingException e) {
-					System.out.println("Error in mapping");
-					e.printStackTrace();
-				} catch (IOException e) {
-					System.out.println("Error in mapping");
-					e.printStackTrace();
-				}
-				System.out.println("onC error");
-				return null;
+				return Futures.immediateFuture(Optional.of(welcomeMessage(event.getUser().getName())));
 			}
 
 		}));
@@ -186,34 +192,85 @@ public class ViberBotServiceImpl implements ViberBotService {
 	}
 
 	private MessageKeyboard createRoomKeyboard() {
-		
-		Map<String,Object> keyboardMap=new HashMap<>();
-		keyboardMap.put("Type","keyboard");
-		List<Room> rooms=roomService.listAll();
+
+		Map<String, Object> keyboardMap = new HashMap<>();
+		keyboardMap.put("Type", "keyboard");
+		List<Room> rooms = roomService.listAll();
 		List<Map> buttons = new ArrayList<>();
 
-		Map<String, Object> cancelButton  = new HashMap<>();
+		Map<String, Object> cancelButton = new HashMap<>();
 		cancelButton.put("Text", "Cancel");
 		cancelButton.put("BgColor", "#f00b0b");
 		cancelButton.put("ActionBody", "Cancel");
-    	buttons.add(cancelButton);
-		
-		for(Room room: rooms) {
-			
-			String roomOnViber=room.getName()+" "+room.getNumber();
-			Map<String, Object> button  = new HashMap<>();
-	    	button.put("BgColor", "#2db9b9");
-	    	button.put("Text", roomOnViber);
-	    	button.put("ActionBody", roomOnViber);
-	    	buttons.add(button);
-				
+		buttons.add(cancelButton);
+
+		for (Room room : rooms) {
+
+			String roomOnViber = room.getName() + " " + room.getNumber();
+			Map<String, Object> button = new HashMap<>();
+			button.put("BgColor", "#2db9b9");
+			button.put("Text", roomOnViber);
+			button.put("ActionBody", roomOnViber);
+			buttons.add(button);
+
 		}
-		
+
 		keyboardMap.put("Buttons", buttons);
-		
+
 		return new MessageKeyboard(keyboardMap);
-		
-	}	
-	
-	
+
+	}
+
+	private MessageKeyboard createCancelKeyboard() {
+
+		Map<String, Object> keyboardMap = new HashMap<>();
+		keyboardMap.put("Type", "keyboard");
+
+		List<Map> buttons = new ArrayList<>();
+
+		Map<String, Object> cancelButton = new HashMap<>();
+		cancelButton.put("Text", "Cancel");
+		cancelButton.put("BgColor", "#f00b0b");
+		cancelButton.put("ActionBody", "Cancel");
+		buttons.add(cancelButton);
+		keyboardMap.put("Buttons", buttons);
+
+		return new MessageKeyboard(keyboardMap);
+
+	}
+
+	private TextMessage welcomeMessage(String userName) {
+
+		String JSONString = "{\"Type\": \"keyboard\",\"Buttons\": [{\"Columns\": 3,\"Rows\": 2,\"Text\": \"<font color=\\\"#494E67\\\">Reserve room </font><br><br>\",\"TextSize\": \"medium\",\"TextHAlign\": \"center\",\"TextVAlign\": \"bottom\",\"ActionType\": \"reply\",\"ActionBody\": \"Reserve room\",\"BgColor\": \"#f7bb3f\"}, {\"Columns\": 3,\"Rows\": 2,\"Text\": \"<font color=\\\"#494E67\\\">See previous reservations</font><br><br>\",\"TextSize\": \"medium\",\"TextHAlign\": \"center\",\"TextVAlign\": \"bottom\",\"ActionType\": \"reply\",\"ActionBody\": \"See previous reservations\",\"BgColor\": \"#f6f7f9\"}]}";
+		String keyboardMessageStr = JSONString;
+
+		try {
+			MessageKeyboard messageKeyboard = new MessageKeyboard(
+					new ObjectMapper().readValue(keyboardMessageStr, new TypeReference<Map<String, Object>>() {
+					}));
+
+			Map<String, Object> trDataHMap = new HashMap<>();
+			trDataHMap.put("message", "welcome_message");
+
+			TrackingData trData = new TrackingData(trDataHMap);
+			String text = "Hello, " + userName
+					+ ". Welcome to ViberBot room reservation service. Please select one of the following options:";
+			Integer minApiVersion = new Integer(1);
+			TextMessage txtMessage = new TextMessage(text, messageKeyboard, trData, minApiVersion);
+			return txtMessage;
+
+		} catch (JsonParseException e) {
+			System.out.println("Error in parsing");
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			System.out.println("Error in mapping");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error in mapping");
+			e.printStackTrace();
+		}
+		System.out.println("onC error");
+		return null;
+	}
+
 }
